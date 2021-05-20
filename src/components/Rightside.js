738f6ff0a6709e652bf14d662fe46b91df9f0c40
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-05-20 19:49:46 +0530

    Leftside completed

--- src/components/Rightside.js
+++ src/components/Rightside.js
@@ -4,7 +4,9 @@ import styled from 'styled-components'
 const Rightside = (props) => {
     return (
         <Container>
-            Right side
+            <FollowCard>
+                rightside
+            </FollowCard>
         </Container>
     )
 }
@@ -13,4 +15,15 @@ export default Rightside
 
 const Container = styled.div`
     grid-area: Rightside;
+`
+const FollowCard = styled.div`
+    text-align: center;
+    overflow: hidden;
+    margin-bottom: 8px;
+    background-color: #fff;
+    border-radius: 5px;
+    position: relative;
+    border: none;
+    box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+    padding: 12px;
 `
